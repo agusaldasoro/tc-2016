c942caa7f1dcddccebc042e08a763c724f88a27e
Agustina <agusaldasoro@gmail.com>
2016-07-21 20:56:03 -0300

    no andaaaaaa

--- Dia3/L.cpp
+++ Dia3/L.cpp
@@ -1,60 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main(){
+    int maximum = 31623;
+    vector<int> listaDePrimos;
+    vector<int> factorizados(maximum, 0);
+    for (int i = 2; i < maximum; i++) {
+        if (factorizados[i] == 0){
+        // Si estoy en un primo
+            listaDePrimos.push_back(i);
+            for (int j = 2*i; j <= maximum; j+=i) {
+                factorizados[j] = 1;
+            }
+        }
+    }
 
-
-	int maximum = 1000000;
-//	vector<vector<int> > factorizados(maximum);
-//	for (int i = 2; i < maximum; i++) {
-//		if (factorizados[i].empty()){
-//		// Si estoy en un primo
-//			for (int j = 2*i; j <= maximum; j+=i) {
-//				factorizados[j].push_back(i);
-//				cout << i << endl;
-//				// Lo agrego a su factorizacion
-//			}
-//		}
-//	}
-
-    	vector<vector<long> > factorizados;
-    	for(int i=0; i<maximum+1; i++){
-            vector<long> aInsertar;
-            factorizados.push_back(aInsertar);
-    	}
-        for (int i = 2; i < maximum; i++) {
-            if (factorizados[i].empty()){
-            // Si estoy en un primo
-                for (int j = 2*i; j <= maximum; j+=i) {
-
-                    factorizados[j].push_back(i);
-                    // Lo agrego a su factorizacion
+	int n, n2, k, w, z, parcial, tamanioInicial;
+	long long multiplicador, finale, sumando, suma;
+	while(cin >> n) {
+        vector<int> descomposicion;
+        n2 = n;
+        for(z=0; z < listaDePrimos.size(); z++){
+            if (n2 < listaDePrimos[z]*listaDePrimos[z] && n2!=1) {
+                descomposicion.push_back(n2);
+                break;
+            }
+            else if (n2%listaDePrimos[z] == 0){
+                descomposicion.push_back(listaDePrimos[z]);
+                while(n2%listaDePrimos[z] == 0){
+                    n2 = n2 / listaDePrimos[z];
                 }
             }
         }
-
-
-
-	int n;
-
-	while(cin >> n) {
-
-		vector<bool> arreglo(n+1, false);
-		int suma = 0;
-
-		for (int i = 0; i < factorizados[n].size(); i++)
-		{
-			for (int k = factorizados[n][i]; i < factorizados[n].size(); i++)
-			{
-				if (arreglo[factorizados[n][i]]== false){
-					arreglo[factorizados[n][i]]== true;
-					suma += factorizados[n][i];
-				}
-			}
-		}
-		cout << suma%1000000007 << endl;
-
+        vector<pair<int, long long> > partes(1, make_pair(-1, 0));
+        for (k = 0; k < descomposicion.size(); k++){
+            tamanioInicial = partes.size();
+            for (w = 0; w < tamanioInicial; w++){
+                multiplicador = abs(partes[w].first*descomposicion[k]);
+                finale = n/multiplicador;
+                sumando = finale*(finale-1)/2;
+                partes.push_back(make_pair((-1)*partes[w].first*descomposicion[k], sumando));
+            }
+        }
+        suma = 0;
+        for (k = 1; k < partes.size(); k++){
+            suma += partes[k].first*partes[k].second;
+        }
+        cout << suma%1000000007 << endl;
 	}
 	return 0;
 }
