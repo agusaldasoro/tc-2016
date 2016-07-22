dd6193e28bd685a17d36a378848ba8735e297575
Francisco Noriega <FranNoriega@users.noreply.github.com>
2016-07-22 17:54:49 -0300

    Update E.cpp

--- Dia-4/E.cpp
+++ Dia-4/E.cpp
@@ -1,27 +1,46 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+set<unsigned long long> primes;
+vector<unsigned long long> numbers;
+vector<unsigned long long> criba;
+
 int main(){
+	unsigned long long number;
+	int maximum = -1;
 	int tests;
 	cin >> tests;
-	int n;
 	for(int i = 0; i < tests; i++){
-		cin >> n;
-		n = 0;
-		cout << "number is " << n << cout;
-		int prime = n;
-		bool notPrime = true;
-		while(notPrime){
-			for(int num = 2; num <= (int) sqrt(prime); num++){
-				if(prime % num == 0){
-					notPrime = false;
-				}
-			}
-			if(notPrime) prime++;
-		}
-		cout << prime << endl;	
+		cin >> number;
+		numbers.push_back(number);
 	}
-	
+	criba.resize(671200,0);
+	primes.insert(2);
+	for (unsigned long long i = 3; i < criba.size(); i+=2) {
+		if (criba[i]==0) {
+			primes.insert(i);
+			for(int j = 3*i; j <= criba.size(); j+=i*2){
+				criba[j]++;
+			}
+		}
+	}
+	for(unsigned int i = 0; i < numbers.size(); i++){
+		unsigned long long minPrime = numbers[i];
+		if(minPrime < 2) minPrime = 2;
+		while(true){
+			auto it = primes.begin();
+			bool isPrime = true;
+			while(it != primes.end() && (*it) < minPrime){
+				if(minPrime % (*it)  == 0){
+					isPrime = false;
+					break;
+				}
+				else it++;
+			}
+			if(isPrime) break;
+			else minPrime++;
+		}
+		cout << minPrime << "\n";
+	}
 }
